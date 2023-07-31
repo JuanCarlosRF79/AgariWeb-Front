faee697bc2db6f8b6da9f154c77f6d1e17852753
JuanCarlosRF79 <juan.ruvalcaba@ia-society.com>
2023-07-30 19:34:11 -0600

    Buscar servicio y mostrarlos en tarjetas

--- src/app/administracion/servicio/servicio.component.ts
+++ src/app/administracion/servicio/servicio.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { ServicioService } from 'src/app/servicios/servicio.service';
+import { ClienteService } from 'src/app/servicios/cliente.service';
 
 @Component({
   selector: 'app-servicio',
@@ -7,40 +9,139 @@ import { Component } from '@angular/core';
 })
 export class ServicioComponent {
 
-  alertaArriba=false;
-  alertaAbajo=false;
-  exito=false;
+  constructor(private servicioSev:ServicioService, 
+    private clienteServ:ClienteService) { }
 
-  descripcionAlert="";
+    ngOnInit(): void {
+      this.consultarClientes()
+      this.todoServ()
+    }
 
-  filtrado:any;
-  filtroEst="";
-  filtroFech="";
+  alertaArriba=false
+  alertaAbajo=false
+  exito=false
 
-  servicios:any;
+  descripcionAlert=""
+
+  filtrado:any
+  filtroEst=""
+  filtroFech=""
+
+  clientes:any
+  maxId:any
+  servicios:any
 
   servicio={
-    codigoTick:"",
-    nombreCli:"",
-    apellidoPatCli:"",
+    idServicio:"",
+    idCliente:"",
     nombreEmp:"",
     apellidoPatEmp:"",
     telefono:"",
-   // descuento: "",        
-    //descuentoDesc: "",
-    metodoPago : "",
-    tipoTicket :  "Servicio",
-    
+    estado: "Jalisco",
+    ciudad: "",
     calle : "",
     colonia : "",
-    descViv : "",
-    descProb : "",
-    fechaSol : "",
-    estado : "",
-    fechaFin :  "",
+    descripcionDireccion : "",
+    descripcionProblema : "",
+    fechaOrden : "",
+    estadoServicio : "",
+    fechaFinalizado :  "",
+    proximaCita :  "",
+    pagoServicio : ""
+  }
 
-    costoTotal : "",
-    fechaTicket : ""
+  consultarClientes(){
+    this.clienteServ.todoCli().subscribe(
+      (res)=>{
+        this.clientes=res
+        console.log(this.clientes)
+      },(err)=>{
+        console.log(err);
+      }
+    );
+  }
+
+  todoServ(){
+    this.servicioSev.todoServ().subscribe(
+      (res)=>{
+        this.servicios=res
+        this.maxId=res.length
+        console.log(this.servicios)
+      },(err)=>{
+        alert(err.error)
+        console.log(err);
+      }
+    );
+  }
+
+  insertarServ(){
+
+  }
+
+  modificarServ(){
+
+  }
+
+  formatoFecha(fecha:any){
+    var apoyo = fecha.split("T")
+    return apoyo[0]
+  }
+
+  formatoCosto(costo:any){
+    return costo.toLocaleString('en-US')
+  }
+
+  formatoTelefono(telefono:any){
+    var apoyo = telefono.slice(0,2)+" "+telefono.slice(2,6)+" "+telefono.slice(6,10)
+    return apoyo
+  }
+
+  buscarServ(idServicio:any){
+    this.servicio.idServicio=idServicio
+    this.servicioSev.consultarServ(this.servicio).subscribe(
+      (res)=>{
+        this.servicio=res[0];
+        
+        var fechas = res[0].fechaOrden.split("T")
+        this.servicio.fechaOrden = fechas[0]
+        fechas=res[0].proximaCita.split("T")
+        this.servicio.proximaCita = fechas[0]
+        fechas=res[0].fechaFinalizado.split("T")
+        this.servicio.fechaFinalizado = fechas[0]
+
+        
+      },(err)=>{
+        alert(err.error)
+        console.log(err);
+      }
+    );
+  }
+
+  consultarServ(){
+    this.servicioSev.consultarServ(this.servicio).subscribe(
+      (res)=>{
+        this.servicio=res[0];
+        
+        var fechas = res[0].fechaOrden.split("T")
+        this.servicio.fechaOrden = fechas[0]
+        fechas=res[0].proximaCita.split("T")
+        this.servicio.proximaCita = fechas[0]
+        fechas=res[0].fechaFinalizado.split("T")
+        this.servicio.fechaFinalizado = fechas[0]
+
+      },(err)=>{
+        alert(err.error)
+        console.log(err);
+      }
+    );
+  }
+
+  eliminarServ(){
+
+  }
+
+  limpiar(){
+
   }
 
 }
---
+++ src/app/servicios/cliente.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  //private url="http://192.168.0.106:3000/clientes";
+  private url="http://localhost:3000/cliente";
+
+  constructor(private http:HttpClient) { }
+
+  consultarCli(cliente:object){
+    return this.http.post<any>(this.url+"/buscar",cliente);
+  }
+
+  insertarCli(cliente:object){
+    return this.http.post<any>(this.url,cliente);
+  }
+
+  modificarCli(cliente:object){
+    return this.http.put<any>(this.url,cliente);
+  }
+
+  eliminarCli(cliente:object){
+    return this.http.post<any>(this.url+"/borrar",cliente);
+  }
+
+  todoCli(){
+    return this.http.get<any>(this.url);
+  }
+  
+}
